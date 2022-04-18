5f709535fe0f50e937a438eceb0ba54894ab7dd7
yugyeong71 <103247384+yugyeong71@users.noreply.github.com>
2022-04-18 22:25:50 +0900

    숫자의 합

---
+++ 11720/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // 숫자 개수 입력
+        String str = sc.next();
+
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            sum += str.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
