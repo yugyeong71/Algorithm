91884a73852a478071a1ff69bef3624da23b8659
yugyeong71 <yugyeong71@naver.com>
2022-04-24 18:55:01 +0900

    Title : 구조 수정 Body : 프로젝트 디렉토리 구조 변경

---
+++ src/algorithm/SortNumber2750.java
@@ -0,0 +1,23 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class SortNumber2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int M[] = new int[N];
+
+        for(int i = 0; i < N; i++){
+             M[i] = sc.nextInt();
+        }
+
+        Arrays.sort(M);
+
+        for (int i : M) {
+            System.out.print(i + "\n");
+        }
+    }
+}
