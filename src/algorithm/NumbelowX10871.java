9ad4a481c772e7ccab9a4c422fb8f6142fdd70b8
yugyeong71 <youkyoung71@naver.com>
2022-04-28 20:47:38 +0900

    백준 10871

---
+++ src/algorithm/NumbelowX10871.java
@@ -0,0 +1,20 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class NumbelowX10871 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+
+        for(int i = 0; i < N; i++){
+            int A = sc.nextInt();
+                if(A < X){
+                    System.out.print(A + " ");
+                }
+        }
+
+    }
+}
