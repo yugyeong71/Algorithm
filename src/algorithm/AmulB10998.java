918a9cb741f3e65c3a343c4472792555f21082e1
yugyeong71 <youkyoung71@naver.com>
2022-05-04 01:15:21 +0900

    백준 10998

---
+++ src/algorithm/AmulB10998.java
@@ -0,0 +1,14 @@
+package com.company.src.algorithm;
+
+import java.util.Scanner;
+
+public class AmulB10998 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+    }
+}
