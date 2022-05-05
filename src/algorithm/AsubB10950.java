2c2e07b586603ebd115e8c510e76365575db57ea
yugyeong71 <youkyoung71@naver.com>
2022-05-06 00:46:58 +0900

    백준 10950

---
+++ src/algorithm/AsubB10950.java
@@ -0,0 +1,24 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class AsubB10950 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int A = 0;
+        int B = 0;
+        int arr[] = new int[T];
+
+        for(int i = 0; i < T; i++){
+            A = sc.nextInt();
+            B = sc.nextInt();
+            arr[i] = A + B;
+        }
+
+        for (int i : arr){
+            System.out.println(i);
+        }
+    }
+}
