7cdede6c7dcfde7bf9248bbe56acba2d91a45f1e
yugyeong71 <youkyoung71@naver.com>
2022-05-08 20:21:19 +0900

    백준 8393

---
+++ src/algorithm/Sum1toN8393.java
@@ -0,0 +1,19 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class Sum1toN8393 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int sum = 0;
+
+        for(int i = 1; i <= N; i++){
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
