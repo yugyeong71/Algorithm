c83e661c689a001a9ddacb27954b0749a55acda2
yugyeong71 <youkyoung71@naver.com>
2022-05-11 14:11:48 +0900

    백준 2753

---
+++ src/algorithm/LeapYear2753.java
@@ -0,0 +1,18 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class LeapYear2753 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        if(year%4 == 0 && year%100 != 0 || year%400 == 0){
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
