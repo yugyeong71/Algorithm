f70e2659a1e744d4274025ac30168ef4717f141d
yugyeong71 <youkyoung71@naver.com>
2022-05-14 01:49:37 +0900

    백준 2742

---
+++ src/algorithm/NtoOne2742.java
@@ -0,0 +1,16 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class NtoOne2742 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = N; i >= 1; i--){
+            System.out.println(i);
+        }
+
+    }
+}
