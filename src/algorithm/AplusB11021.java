2ea78617d60eb47598d2a6601d2029a65e2105b4
yugyeong71 <youkyoung71@naver.com>
2022-05-15 14:31:04 +0900

    백준 11021

---
+++ src/algorithm/AplusB11021.java
@@ -0,0 +1,18 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class AplusB11021 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        for (int i = 1; i <= test; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            System.out.println("Case #" + i + ": " + (A + B));
+        }
+    }
+}
