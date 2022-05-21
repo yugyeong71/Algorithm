0eb9d0f78f259a85881c15036ff37bcfe3767634
고유경 <youkyoung71@naver.com>
2022-05-21 16:41:17 +0900

    백준 10951

---
+++ src/algorithm/AplusB10951.java
@@ -0,0 +1,15 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class AplusB10951 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextInt()){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+    }
+}
