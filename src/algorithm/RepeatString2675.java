b7ce72d119d1893939f7ed42a701517db680caee
고유경 <youkyoung71@naver.com>
2022-05-26 23:40:49 +0900

    백준 2675

---
+++ src/algorithm/RepeatString2675.java
@@ -0,0 +1,29 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class RepeatString2675 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+        int N = 0;
+        String[] str = new String[test];
+        String s;
+        for(int i = 0; i < test; i++){
+            N = sc.nextInt();
+            s = sc.next();
+            String[] split = s.split("");
+            str[i] = "";
+
+            for(int j = 0; j < s.length(); j++){
+                for (int k = 0; k < N; k++){
+                    str[i] += split[j];
+                }
+            }
+        }
+        for (String S: str){
+            System.out.println(S);
+        }
+    }
+}
