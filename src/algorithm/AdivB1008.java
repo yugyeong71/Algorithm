962433212dda01e9033075596193c791758d1d51
고유경 <youkyoung71@naver.com>
2022-06-01 16:41:48 +0900

    백준 1008

---
+++ src/algorithm/AdivB1008.java
@@ -0,0 +1,15 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class AdivB1008 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A/B);
+
+    }
+}
