2893bb8ee2f043964de51e0df7a0c35ede6c9f04
고유경 <youkyoung71@naver.com>
2022-06-07 20:50:47 +0900

    백준 1065

---
+++ src/algorithm/EvenSequence1065.java
@@ -0,0 +1,34 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class EvenSequence1065 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(Even_sequence(sc.nextInt()));
+    }
+
+    public static int Even_sequence(int num) {
+        int cnt = 0;
+
+        if (num < 100) {
+            return num;
+        }
+
+        else {
+            cnt = 99;
+
+            for (int i = 100; i <= num; i++) {
+                int hun = i / 100;
+                int ten = (i / 10) % 10;
+                int one = i % 10;
+
+                if ((hun - ten) == (ten - one)) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
