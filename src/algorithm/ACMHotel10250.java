e030a01e95f3d9e12611474d8318cca3d7b99aea
고유경 <youkyoung71@naver.com>
2022-07-02 16:10:04 +0900

    백준 10250

---
+++ src/algorithm/ACMHotel10250.java
@@ -0,0 +1,31 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class ACMHotel10250 {
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        for (int i = 0; i < test; i++) {
+
+            int height = sc.nextInt();
+            int width = sc.nextInt();
+            int customer =sc.nextInt();
+
+            int count =0;
+
+            int [] save = new int[10000];
+
+            for (int j = 1; j <= width; j++) {
+                for(int k = 1; k <= height; k++) {
+                    save[count]=( (k * 100) + j );
+                    count++;
+                }
+            }
+            System.out.println(save[customer - 1]);
+        }
+    }
+}
