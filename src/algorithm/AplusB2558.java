388286c475a581fadfcf251a9873cf38698daf31
고유경 <youkyoung71@naver.com>
2022-07-08 17:17:18 +0900

    백준 2558

---
+++ src/algorithm/AplusB2558.java
@@ -0,0 +1,16 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class AplusB2558 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int Sum = A + B;
+
+        System.out.println(Sum);
+    }
+}
