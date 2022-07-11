8568f06eb1d2ba38f90844afbff14be8a7c3dfb3
고유경 <youkyoung71@naver.com>
2022-07-11 23:07:18 +0900

    백준 2420

---
+++ src/algorithm/SafariWorld2420.java
@@ -0,0 +1,15 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class SafariWorld2420 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int Sum = Math.abs(A) + Math.abs(B);
+
+        System.out.println(Sum);
+    }
+}
