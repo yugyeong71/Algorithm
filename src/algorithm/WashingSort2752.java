d643fd21464fd22b00c22791dadb212412a4ba96
고유경 <youkyoung71@naver.com>
2022-07-15 23:24:34 +0900

    백준 2752

---
+++ src/algorithm/WashingSort2752.java
@@ -0,0 +1,22 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class WashingSort2752 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for(int j : arr){
+            System.out.print(j + " ");
+        }
+
+    }
+}
