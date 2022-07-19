69a10ea04eb2f7e5cdc22ead2c8dfdca44558e1d
고유경 <youkyoung71@naver.com>
2022-07-19 20:32:16 +0900

    백준 2693

---
+++ src/algorithm/NthNumber2693.java
@@ -0,0 +1,22 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class NthNumber2693 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int test = sc.nextInt();
+
+        int[] num = new int[10];
+
+        for(int i = 0; i < test; i++){
+            for(int j = 0; j < num.length; j++){
+                num[j] = sc.nextInt();
+            }
+            Arrays.sort(num);
+            System.out.println(num[7]);
+        }
+
+    }
+}
