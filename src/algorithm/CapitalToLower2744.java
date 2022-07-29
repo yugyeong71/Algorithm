e934b0c8b058443d47e75333beba3c0c6abc0736
고유경 <youkyoung71@naver.com>
2022-07-29 12:01:52 +0900

    백준 2744

---
+++ src/algorithm/CapitalToLower2744.java
@@ -0,0 +1,25 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class CapitalToLower2744 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        char[] arr;
+        arr = str.toCharArray();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(65 <= arr[i] && arr[i] <= 90) {
+                arr[i] = (char) (arr[i] + 32);
+                continue;
+            }
+            if(97 <= arr[i] && arr[i] <= 122) {
+                arr[i] = (char) (arr[i] - 32);
+                continue;
+            }
+        }
+        System.out.println(arr);
+    }
+}
