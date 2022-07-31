f055707e522ef9bd215561fb52416d2cf55300c1
고유경 <youkyoung71@naver.com>
2022-07-31 21:37:44 +0900

    백준 2522

---
+++ src/algorithm/PrintStar2522.java
@@ -0,0 +1,32 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class PrintStar2522 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            for (int j=n-i; j>=1; j--) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        for (int i=1; i<=n-1; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print(" ");
+            }
+            for (int j=n-i; j>=1; j--) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
