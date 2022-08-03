25bf4879c508178c8f70dcaaa265a13357ae381f
고유경 <youkyoung71@naver.com>
2022-08-03 20:38:19 +0900

    백준 10569

---
+++ src/algorithm/Polyhedron10569.java
@@ -0,0 +1,20 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class Polyhedron10569 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++){
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+
+            int side = -(V - E) + 2;
+
+            System.out.println(side);
+        }
+    }
+}
