99106348fa2626f6bda425778fb8605197d8c6bc
고유경 <youkyoung71@naver.com>
2022-08-10 00:41:33 +0900

    백준 4470

---
+++ src/algorithm/LineNumber4470.java
@@ -0,0 +1,17 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class LineNumber4470 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String text[] = new String[N];
+
+        for(int i = 1; i <= N; i++){
+            text[i] = sc.next();
+            System.out.println(i + ". " + text[i]);
+        }
+    }
+}
