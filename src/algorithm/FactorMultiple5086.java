dcab23ea91da7ff95afb9ae06d1ea7513ad54b49
고유경 <youkyoung71@naver.com>
2022-08-09 23:34:12 +0900

    백준 5086

---
+++ src/algorithm/FactorMultiple5086.java
@@ -0,0 +1,30 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class FactorMultiple5086 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            if(N == 0 && M == 0){
+                break;
+            }
+
+            else if(M % N == 0){
+                System.out.println("factor");
+            }
+
+            else if(N % M == 0){
+                System.out.println("multiple");
+            }
+
+            else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
