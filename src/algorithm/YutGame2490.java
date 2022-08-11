2b6e74a7ac25f9cec24cf4cc7ae8d4ac053012f5
고유경 <youkyoung71@naver.com>
2022-08-11 22:20:13 +0900

    백준 2490

---
+++ src/algorithm/YutGame2490.java
@@ -0,0 +1,40 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class YutGame2490 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int yut[] = new int[4];
+        int count = 0;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < yut.length; j++){
+                yut[i] = sc.nextInt();
+
+                if(yut[i] == 1){
+                    count ++;
+                }
+            }
+            switch (count){
+                case 0:
+                    System.out.println("D");
+                    break;
+                case 1:
+                    System.out.println("C");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("A");
+                    break;
+                case 4:
+                    System.out.println("E");
+                    break;
+            }
+            count = 0;
+        }
+    }
+}
