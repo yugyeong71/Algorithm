2d959770ab97dc30a35fa8f19b6f6ff3dc75af33
고유경 <youkyoung71@naver.com>
2022-09-25 16:11:47 +0900

    백준 25305

---
+++ src/algorithm/CutLine25305.java
@@ -0,0 +1,22 @@
+package com.company.src.algorithm;
+
+import java.util.*;
+
+public class CutLine25305 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        Integer[] student = new Integer[N];
+
+        for(int i = 0; i < N; i++){
+            student[i] = sc.nextInt();
+        }
+
+        Arrays.sort(student, Collections.reverseOrder());
+
+        System.out.println(student[K-1]);
+
+    }
+}
