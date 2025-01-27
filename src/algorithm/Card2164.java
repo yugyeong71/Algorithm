4f418a9c534dc81f9633798a6fc15e292b86ebb5
GoYuGyeong <yugyeong@kai-i.com>
2025-01-27 21:39:38 +0900

    카드2 2164

---
+++ src/algorithm/Card2164.java
@@ -0,0 +1,31 @@
+package com.company.src.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Card2164 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(card2(N));
+	}
+
+	private static int card2(int N) {
+		Queue<Integer> queue = new LinkedList<>();
+
+		for (int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+
+		while (queue.size() > 1) {
+			queue.remove();
+			queue.add(queue.remove());
+		}
+
+		return queue.peek();
+	}
+}
