fd6943fcbf56166208eaf9264ba97f3fe66baeb8
GoYuGyeong <yugyeong@kai-i.com>
2025-01-29 18:44:30 +0900

    짐 챙기는 숌 1817

---
+++ src/algorithm/PackingShom1817.java
@@ -0,0 +1,38 @@
+package com.company.src.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PackingShom1817 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		if (N == 0) {
+			System.out.println(0);
+			return;
+		}
+
+		st = new StringTokenizer(br.readLine());
+
+		int boxWeight = 0, boxCount = 1;
+
+		for (int i = 0; i < N; i++) {
+			int weight = Integer.parseInt(st.nextToken());
+			if (boxWeight + weight > M) {
+				boxCount++;
+				boxWeight = 0;
+			}
+			boxWeight += weight;
+		}
+
+		System.out.println(boxCount);
+	}
+
+}
