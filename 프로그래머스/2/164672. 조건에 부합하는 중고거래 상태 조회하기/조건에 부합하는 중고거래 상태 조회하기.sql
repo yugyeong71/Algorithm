f5823187f512437bc77a3a5fad2dd363ab938ed2
Yu Gyeong <youkyoung71@naver.com>
2025-07-14 14:12:54 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,8 @@
+SELECT ugb.BOARD_ID, ugb.WRITER_ID, ugb.TITLE, ugb.PRICE, CASE ugb.STATUS
+        WHEN 'SALE' THEN '판매중'
+        WHEN 'RESERVED' THEN '예약중' 
+        WHEN 'DONE' THEN '거래완료'
+        END AS 'STATUS'
+FROM USED_GOODS_BOARD ugb
+WHERE ugb.CREATED_DATE = '2022-10-05'
+ORDER BY ugb.BOARD_ID DESC
