cf86ab381815bbe2efe00f1aea4b0c4d3345723f
Yu Gyeong <youkyoung71@naver.com>
2025-11-08 21:52:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.66 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack();
+        
+        for (int i : arr) {
+            if (stack.empty() || stack.peek() != i) {
+                stack.push(i);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        
+        for (int i = answer.length - 1; i >= 0 ; i--) {
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
